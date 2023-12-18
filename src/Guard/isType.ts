db12943219b67ae2b65c5ba46cbebc1f351dc4dc
13OnTheCode <13@ryongyon.com>
2023-12-18 22:56:02 +0800

    fix: rename `isType` to `IsType`

--- src/Guard/isType.ts
+++
@@ -1,9 +0,0 @@
-import type { IsAny } from './IsAny'
-import type { IsNever } from './IsNever'
-import type { IsUnknown } from './IsUnknown'
-
-export type IsType<T1, T2> = (
-  [T1, IsAny<T1> | IsNever<T1> | IsUnknown<T1>] extends [T2, false]
-    ? true
-    : false
-)
