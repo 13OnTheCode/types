fd579897d67d5dd01f06c45918ff8ad9dc67277f
13OnTheCode <13@ryongyon.com>
2023-12-26 05:41:07 +0800

    refactor: optimize `GetIndexs` logic

--- src/Array/GetIndexs.ts
+++ src/Array/GetIndexs.ts
@@ -1,7 +1,12 @@
 import type { UnknownArray } from '../Base/UnknownArray'
+import type { IsTuple } from '../Guard/IsTuple'
 
 export type GetIndexs<T extends UnknownArray> = (
   T extends UnknownArray
-    ? Exclude<Partial<T>['length'], T['length']>
+    ? [IsTuple<T>, Exclude<Partial<T>['length'], Required<T>['length']>] extends [true, infer Indexs]
+      ? [Indexs] extends [never]
+        ? number
+        : Indexs
+      : number
     : never
 )
