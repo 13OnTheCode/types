833e1d306b89383059eb82931d1a8f82e993f6f0
13OnTheCode <13@ryongyon.com>
2023-12-27 04:32:17 +0800

    refactor: optimize `IsTuple` logic

--- src/Guard/IsTuple.ts
+++ src/Guard/IsTuple.ts
@@ -3,7 +3,7 @@ import type { UnknownArray } from '../Base/UnknownArray'
 import type { IsExtends } from './IsExtends'
 
 export type IsTuple<T> = (
-  [IsExtends<T, UnknownArray>, IsExtends<never[], T>] extends [true, false]
+  [IsExtends<Required<T>, UnknownArray>, IsExtends<never[], Required<T>>] extends [true, false]
     ? true
     : false
 )
